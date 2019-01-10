c13af6872b6ec5389b349b3d6e4aca00e999680c
Saša Barišić <cartman300@net.hr>
2019-01-10 10:13:44 +0100

    Password sample

--- SampleWebApp/Page.aspx
+++ SampleWebApp/Page.aspx
@@ -10,9 +10,19 @@
 	<link rel="stylesheet" href="content/page.css" />
 </head>
 <body>
-	<form id="form1" runat="server">
-		<div>
+	<form class="form-inline center-form" runat="server">
+		<div class="form-group mb-2">
+			<label for="staticEmail2" class="sr-only">Email</label>
+			<input type="text" readonly="readonly" class="form-control-plaintext" id="staticEmail2" value="email@example.com" />
 		</div>
+
+		<div class="form-group mx-sm-3 mb-2" runat="server">
+			<label for="inputPassword2" class="sr-only">Password</label>
+			<input type="password" class="form-control" id="inputPassword2" placeholder="Password" runat="server" />
+		</div>
+
+		<asp:Button CssClass="btn btn-primary mb-2" runat="server" OnClick="OnConfirm" Text="Confirm identity" />
+		<span id="status_label" runat="server">Success</span>
 	</form>
 
 	<script src="content/js/bootstrap.bundle.js"></script>
--- SampleWebApp/Page.aspx.cs
+++ SampleWebApp/Page.aspx.cs
@@ -8,7 +8,33 @@ using System.Web.UI.WebControls;
 namespace SampleWebApp {
 	public partial class Page : System.Web.UI.Page {
 		protected void Page_Load(object sender, EventArgs e) {
+			status_label.Visible = false;
+		}
 
+		protected void OnConfirm(object sender, EventArgs e) {
+			string Password = inputPassword2.Value;
+
+			if (string.IsNullOrEmpty(Password)) {
+				WriteError("Please enter password");
+				return;
+			}
+
+			if (Password == "root")
+				WriteSuccess("Success!");
+			else
+				WriteError("Password is incorrect");
+		}
+
+		void WriteSuccess(string Txt) {
+			status_label.InnerText = Txt;
+			status_label.Attributes.Add("class", "badge badge-success");
+			status_label.Visible = true;
+		}
+
+		void WriteError(string Txt) {
+			status_label.InnerText = Txt;
+			status_label.Attributes.Add("class", "badge badge-danger");
+			status_label.Visible = true;
 		}
 	}
 }
